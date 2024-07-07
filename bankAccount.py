e937ec98ac324d30d49580cd06d37cfc9e806686
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-07-07 09:07:54 +0300

    encapsulate variabel

--- bankAccount.py
+++ bankAccount.py
@@ -1,14 +1,12 @@
 import random
 import string
 class BankAccount:
-    initialBalance = 0
-    accountNumber = 0
     usedAccountNumber = set()
 
-    def __init__ (self, account_holder:str , initial_balance:int ):
+    def __init__ (self, account_holder:str , initial_balance = 0 ):
         self.account_holder = account_holder
-        self.initialBalance = initial_balance 
-        self.accountNumber = self.generateAccountNumber()
+        self.__initialBalance = initial_balance 
+        self.accountNumber = self.generateAccountNumber() 
                 
 
     def generateAccountNumber (self):
@@ -19,17 +17,17 @@ class BankAccount:
                 return account_number
 
     def deposit (self, depositMoney:int):
-        self.initialBalance = self.initialBalance + depositMoney
-        return self.initialBalance
+        self.__initialBalance = self.__initialBalance + depositMoney
+        return self.__initialBalance
 
     def withdraw(self, withdrawMoney:int):
-        if withdrawMoney > self.initialBalance:
+        if withdrawMoney > self.__initialBalance:
             raise Exception("Sorry, You are broke go to work!")
-        self.initialBalance = self.initialBalance - withdrawMoney
-        return self.initialBalance
+        self.__initialBalance = self.__initialBalance - withdrawMoney
+        return self.__initialBalance
 
     def get_balance(self):
-        return f"Your Balance is :{self.initialBalance}"
+        return f"Your Balance is :{self.__initialBalance}"
 
     def get_account_holder(self):
         return f"Account holder name is : {self.account_holder}"
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from bankAccount import *
+from art import *
+from colorama import *
+from numpy import *
+account1 = BankAccount("Ali")
+account2 = BankAccount("khalid")
 
-account1 = BankAccount("Ali", 15)
-account2 = BankAccount("khalid",20)
-
+print(account1.get_balance())
 print(account1.deposit(15))
 print(account1.withdraw(10))
 print(account1.get_balance())
 print(account1.get_account_holder())
 print(account1.get_account_number())
-print(account2.get_account_number())
+print(Fore.BLUE+account2.get_account_number())
